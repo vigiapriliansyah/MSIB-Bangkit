9fcc82487eca7a6dedb01064e02befb9aa7a0dfe
vigiapriliansyah <108187053+vigiapriliansyah@users.noreply.github.com>
2024-09-15 16:49:30 +0700

    Update login-script.js

--- Aplikasi-Login/assets/scripts/login-script.js
+++ Aplikasi-Login/assets/scripts/login-script.js
@@ -1,32 +1,39 @@
-/*
- * Catatan:
- * Gunakan fungsi goToHome() untuk menampilkan halaman home
- * Gunakan fungsi goToLogin() untuk menampilkan halaman login
- * Gunakan fungsi showPopUp() untuk menampilkan pop up error
- */
-
-/* Comment : Membuat variabel untuk setiap elemen view. */
+/** @module Login-Script */
+/** 
+* Membuat variabel loginFormElement untuk tampilan form.
+* @constant {HTMLElement}
+*/
 const loginFormElement = document.querySelector("#loginForm");
 const inputEmailElement = document.querySelector("#inputEmail");
 const inputPasswordElement = document.querySelector("#inputPassword");
 
-/* Comment : Membuat variabel untuk menyimpan informasi email dan password. */
+/** 
+* Membuat variabel inputEmailElement untuk tampilan input email.
+* @constant {HTMLElement}
+*/
 const expectedEmail = "admin@dicoding.com";
+
+/** 
+ * Membuat variabel expectedPassword untuk menyimpan informasi password sementara.
+ * @constant {string}
+ */
 const expectedPassword = "superpassword";
 
-/* Comment : Menambahkan aksi Klik pada Button*/
+/* Comment :  Menambahkan aksi klik pada button. */
 loginFormElement.addEventListener("submit", function (event) {
   event.preventDefault();
-  // TODO 1 : Mendapatkan input email dan password pengguna dari form.
-  
-    /* Comment : Mendapatkan nilai dari masing-masing input (email dan password) ketika tombol ditekan. */
+    /** 
+   * Membuat variabel email untuk menyimpan nilai email yang didapatkan saat button ditekan.
+   * @constant {string}
+   */
   const email = inputEmailElement.value;
+
+   /** 
+   * Membuat variabel password untuk menyimpan niali password yang didapatkan saat button ditekan.
+   * @constant {string}
+   */
   const password = inputPasswordElement.value;
 
-  /* TODO 2 : Buat Logika perbandingan dengan kondisi:
-       Jika variabel email identik dengan expectedEmail dan password identik dengan expectedPassword, panggil fungsi goToHome().
-       Jika tidak, maka panggil fungsi showPopUp().
-     */
 
     /* Comment : Memastikan bahwa nilai email dan password sesuai dengan nilai yang tersimpan. */
   if (email == expectedEmail && password == expectedPassword) {
