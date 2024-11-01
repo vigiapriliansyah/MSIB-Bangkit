859e37df2463b6b8dc41ed507e495018e78cd351
vigiapriliansyah <apriliansyahvigi@gmail.com>
2024-11-02 05:44:41 +0700

    latihan export dan import

---
+++ latihan-js/export.mjs
@@ -0,0 +1,9 @@
+// export default function myFunction() {
+//   console.log("ini fungsi export");
+// }
+
+const name = "vigi";
+const email = "example@gmail.com";
+const age = 20;
+
+export { name, email, age };
---
+++ latihan-js/import.mjs
@@ -0,0 +1,10 @@
+// import myFunction from "./export.mjs";
+//
+// myFunction();
+
+// import { name, email, age } from "./export.mjs";
+import * as variable from "./export.mjs";
+
+console.log(variable.name);
+console.log(variable.email);
+console.log(variable.age);
