6c4af3fbe65c5b92d2ef7019f417162c03646a5f
vigiapriliansyah <apriliansyahvigi@gmail.com>
2024-11-06 20:34:22 +0700

    update latihan nodejs

---
+++ basic-nodejs/app.js
@@ -0,0 +1,12 @@
+//const cpuInformation = process.memoryUsage();
+//
+//console.log(cpuInformation);
+
+// const firstName = process.argv[2];
+// const lastName = process.argv[3];
+//
+// console.log(`Hello ${firstName} ${lastName}`)
+
+// const coffee = require("./coffee");
+//
+// console.log(coffee);
